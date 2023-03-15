9f5156cca2ba43ce20fb5f3f0dfecff6a3082219
taube-a <Taube.Anastasia@yandex.ru>
2023-03-15 16:15:06 +0300

    refactor: improve style by pep8

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -2,7 +2,6 @@ from django.core.exceptions import ValidationError
 from django.shortcuts import get_object_or_404
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
-
 from reviews.models import Category, Comment, Genre, Review, Title, User
 
 
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -10,7 +10,6 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
-
 from api.filters import TitleFilter
 from reviews.models import Category, Genre, Review, Title, User
 
